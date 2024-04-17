a0845a876dca7f719d8267defc3287e4ddaf77f4
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-04-17 09:46:33 +0700

    install next ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from "@nextui-org/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button color="secondary">Secondary</Button>
+    </>
+  );
 }
 
 export default App;
